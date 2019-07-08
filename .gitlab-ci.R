fe6cc0786c8e6e62d3562be63fd56eaea01cde7a
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-07-08 10:01:13 +0200

    Updated .gitlab files

--- .gitlab-ci.R
+++ .gitlab-ci.R
@@ -1,28 +1,26 @@
-install_if_needed <- function(pkg, repos, quiet) {
-  package_path <- find.package(pkg, quiet = quiet)
-  if (length(package_path) == 0) {
+installIfNeeded <- function(pkg,
+                            repos,
+                            quiet) {
+  pkgPath <- find.package(pkg, quiet = quiet)
+  if (length(pkgPath) == 0) {
     message("NOTE: pkg ", pkg, " missing, installing...")
     install.packages(pkg, repos = repos, quiet = quiet)
+  } else {
+    update.packages(pkg, repos = repos, ask = FALSE)
   }
 }
 
-gl_update_pkg_all <- function(repos = "https://cran.rstudio.com",
-                              quiet = TRUE,
-                              install_pkgdown = FALSE) {
-  # update existing
-  update.packages(ask = FALSE, repos = repos, quiet = quiet)
-
-  install_if_needed(pkg = "devtools", repos = repos, quiet = quiet)
-  if (install_pkgdown == TRUE) {
-    install_if_needed(pkg = "pkgdown", repos = repos, quiet = quiet)
+pkgsUpdate <- function(repos = "https://cran.rstudio.com",
+                       quiet = TRUE,
+                       instPkgdown = FALSE) {
+  installIfNeeded(pkg = "remotes", repos = repos, quiet = quiet)
+  if (instPkgdown) {
+    installIfNeeded(pkg = "pkgdown", repos = repos, quiet = quiet)
   }
-
-  devtools::install_dev_deps(repos = repos, quiet = quiet, upgrade = TRUE)
-
+  remotes::install_deps(repos = repos, dependencies = TRUE, quiet = quiet)
   cat("INSTALLED:\n")
   instld <- as.data.frame(installed.packages())
   rownames(instld) <- NULL
   print(instld[, c("Package", "Version")])
-
   return(invisible(TRUE))
 }
