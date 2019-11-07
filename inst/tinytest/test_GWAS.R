ab1b5937f9b3ebf040d0004417e31652ad3c4450
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-11-07 12:32:59 +0100

    Added tests for summary GWAS

---
+++ inst/tinytest/test_GWAS.R
@@ -0,0 +1,40 @@
+load(file = "testdata.rda")
+
+### Test summary.GWAS
+
+## Run some GWAS analsyis for testing.
+
+stg0 <- runSingleTraitGwas(gData = gDataTest, trials = 1)
+stg1 <- runSingleTraitGwas(gData = gDataTest, traits = 2, trials = "ph1", 
+                           thrType = "small", nSnpLOD = 1)
+stgM0 <- runSingleTraitGwas(gData = gDataTest, trials = 1, GLSMethod = "multi")
+
+expect_error(summary(stg, trials = TRUE),
+             "trials should be a character or numeric vector")
+expect_error(summary(stg, trials = 7),
+             "All trials should be in object")
+
+sumStg0 <- capture.output(summary(stg0))
+expect_true(any(grepl(pattern = "Traits analysed: X1, X2, X3, X4, X5", 
+                      x = sumStg0)))
+expect_true(any(grepl(pattern = "Data are available for 3 SNPs", x = sumStg0)))
+expect_true(any(grepl(pattern = "0 of them were not analyzed", x = sumStg0)))
+expect_true(any(grepl(pattern = "Genetic variance: 0.02059749", x = sumStg0)))
+expect_true(any(grepl(pattern = "Residual variance: 1.854127", x = sumStg0)))
+expect_true(any(grepl(pattern = "LOD-threshold: 1.778151", x = sumStg0)))
+expect_true(any(grepl(pattern = "No significant SNPs found", x = sumStg0)))
+expect_true(any(grepl(pattern = "No genomic control correction", x = sumStg0)))
+expect_true(any(grepl(pattern = "inflation-factor: 0.772", x = sumStg0)))
+
+sumStg1 <- capture.output(summary(stg1))
+expect_true(any(grepl(pattern = "Number of significant SNPs: 1", x = sumStg1)))
+expect_true(any(grepl(pattern = "Smallest p-value among the significant SNPs: 0.5564107",
+                      x = sumStg1)))
+expect_true(any(grepl(pattern = "Largest p-value among the significant SNPs: 0.5564107",
+                      x = sumStg1)))
+expect_true(any(grepl(pattern = "LOD-score: 0.2546045", x = sumStg1)))
+
+sumStgM0 <- capture.output(summary(stgM0))
+expect_false(any(grepl(pattern = "Genetic variance", x = sumStgM0)))
+expect_false(any(grepl(pattern = "Residual variance", x = sumStgM0)))
+
