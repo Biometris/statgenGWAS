5dcf74b54730149c3038157789d170458787ba19
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-01-02 12:15:39 +0100

    Cleaned up zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,3 @@
-.onLoad <- function(libname = find.package("statgenGWAS"),
-                    pkgname = "statgenGWAS"){
-  ## CRAN Note avoidance.
-  if (getRversion() >= "2.15.1") {
-    utils::globalVariables("j")
-  }
-  invisible()
-}
-
 .onUnload <- function(libpath) {
   library.dynam.unload("statgenGWAS", libpath)
 }
