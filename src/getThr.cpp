8a18ad2820cb6af18232d76d8d99363b8e8ba26d
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-01-27 10:53:43 +0100

    Improved check for number of cores to use with openmp

--- src/getThr.cpp
+++ src/getThr.cpp
@@ -10,11 +10,16 @@ using namespace Rcpp;
 int getThr(Rcpp::Nullable<Rcpp::IntegerVector> nCores = R_NilValue) {
   int nThr = 1;
 #ifdef _OPENMP
+  // get thread limit from OMP_THREAD_LIMIT. Set to 2 by CRAN. INT_MAX if unset.
+  int thrLim = omp_get_thread_limit();  
+  // get max number of threads from OMP_NUM_THREADS. 
+  // Initialized when OpenMP is started.
   int maxThr = omp_get_max_threads();
+  int maxThrUsed = std::min(thrLim, maxThr);
   if (nCores.isNotNull()) {
-    nThr = std::min(IntegerVector(nCores)[0], maxThr);
+    nThr = std::min(IntegerVector(nCores)[0], maxThrUsed);
   } else {
-    nThr = maxThr - 1;
+    nThr = maxThrUsed - 1;
   }
 #endif
   return nThr;
