d7ff5d3f17196f299de3071c30d58d84f37330c5
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2023-02-21 11:25:28 +0100

    Improved annotation

--- R/annotateSNP.R
+++ R/annotateSNP.R
@@ -7,41 +7,49 @@ annotateSNP <- function(GWAS,
                         gff) {
   signSnp <- do.call(rbind, GWAS$signSnp)
   ## Read gff (https://en.wikipedia.org/wiki/General_feature_format).
-  gffDat <- data.table::fread(gff, skip = 1, na.strings = c("###", "."), 
-                              fill = TRUE, sep = "\t")
-  ## Set colnames as described on wiki.
-  colnames(gffDat) <- c("sequence", "source", "feature", "start", "end", "score",
-                        "strand", "phase", "attributes")
+  # gffDat <- data.table::fread(gff, skip = 1, na.strings = c("###", "."), 
+  #                             fill = TRUE, sep = "\t")
+  # ## Set colnames as described on wiki.
+  # colnames(gffDat) <- c("seqid", "source", "type", "start", "end", "score",
+  #                       "strand", "phase", "attributes")
+  
+  gffDat <- ape::read.gff(gff)
+  gffDat <- data.table::as.data.table(gffDat)
+  
   ## Remove empty sequences and select genes.
-  gffDat <- gffDat[!is.na(gffDat[["sequence"]]), ]
-  gffDat <- gffDat[gffDat[["feature"]] == "gene"]
+  gffDat <- gffDat[!is.na(gffDat[["seqid"]]), ]
+  gffDat <- gffDat[gffDat[["type"]] == "gene", ]
   ## Replace chromosomes by their numeric value.
   ## This might give warnings for non-numeric chromosomes.
   ## These are converted to NA and have to be removed.
-  gffDat[["chr"]] <- suppressWarnings(as.numeric(gsub(pattern = "Chr", 
-                                                      replacement = "",
-                                                      x = gffDat[["sequence"]])))
+  gffDat[["chr"]] <- gsub(pattern = "Chr", replacement = "",
+                          x = gffDat[["seqid"]])
   gffDat <- gffDat[!is.na(gffDat[["chr"]]), ]
+  
+  signSnp[["chr"]] <- gsub(pattern = "chr", replacement = "",
+                           x = signSnp[["chr"]])
+  
   ## Not allocating memory causes an error in the merge step.
   ## This is a known problem in data.table.
   data.table::setalloccol(gffDat, 2048)
   annoDat <- gffDat[signSnp, on = c("chr", "start <= pos", "end >= pos"), 
-                    nomatch = NA]
+                    nomatch = NULL]
   ## Process attributes to extract gene IDs.
   ## Attributes are assumed to be a ; separated list with items of the form a=b. 
   subAttribs <- strsplit(annoDat[["attributes"]], split = ";")
-  annoDat[["ID"]] <- sapply(X = subAttribs, FUN = function(subAttrib) {
+  attrDat <- sapply(X = subAttribs, FUN = function(subAttrib) {
     if (!is.na(subAttrib[1])) {
       items <- strsplit(subAttrib, split = "=")
       items <- setNames(sapply(items, `[[`, 2), sapply(items, `[[`, 1))
-      items[["ID"]]
+      as.data.frame(t(items))
     } else {
-      NA
+      data.frame()
     }
   })
+  attrDat <- do.call(dfBind, args = list(dfList = attrDat))
+  annoDat <- cbind(annoDat, attrDat)
   ## Convert to list with genes per SNP.
-  annoDat2 <- split(annoDat[["ID"]], annoDat[["snp"]])
-  return(annoDat2)
+  return(annoDat)
 }
 
 
